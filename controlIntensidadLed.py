cbe51cbf530a87e10ab25f93ac8823499dd20786
Flextus <benjasg10@gmail.com>
2024-10-18 14:51:23 -0600

    003_Benjamin_1810
    Control de intensidad  de un led usando PWM y botones virtuales

---
+++ controlIntensidadLed.py
@@ -0,0 +1,12 @@
+from machine import Pin, PWM, UART
+led = PWM(Pin(2), freq = 4000000, duty = 0) 
+serial = UART(2, 9600)
+
+while True:
+    if serial.any() > 0: 
+        mensaje = serial.read()
+        print(mensaje)
+        mensaje = int(mensaje)
+        led.duty(mensaje)
+        
+    
